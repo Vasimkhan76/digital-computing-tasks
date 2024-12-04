f1f598be30531d53bba28d8c638f808e147f3720
Vasimkhan76 <majidmansuri050@gmail.com>
2024-12-04 12:46:07 +0530

    task calculator

---
+++ calc.js
@@ -0,0 +1,62 @@
+
+var myinputbox = document.getElementById("inputbox")
+
+
+function in1() {
+    myinputbox.value += "1" 
+}
+function in2() {
+    myinputbox.value += "2" 
+}
+function in3() {
+    myinputbox.value += "3" 
+}
+function in4() {
+    myinputbox.value += "4" 
+}
+function in5() {
+    myinputbox.value += "5" 
+}
+function in6() {
+    myinputbox.value += "6" 
+}
+function in7() {
+    myinputbox.value += "7" 
+}
+function in8() {
+    myinputbox.value += "8" 
+}
+function in9() {
+    myinputbox.value += "9" 
+}
+function in0() {
+    myinputbox.value += "0" 
+}
+function add() {
+    myinputbox.value += "+" 
+}
+function sub() {
+    myinputbox.value += "-" 
+}
+function mult() {
+    myinputbox.value += "*" 
+}
+function div() {
+    myinputbox.value += "/" 
+}
+function eq() {
+    myinputbox.value = eval(
+        myinputbox.value) 
+}
+function sin() {
+    myinputbox.value = Math.sin(myinputbox.value)
+}
+function cos() {
+    myinputbox.value = Math.cos(myinputbox.value)
+}
+function tan() {
+    myinputbox.value = Math.tan(myinputbox.value)
+}
+function ac() {
+    myinputbox.value = ""
+}
